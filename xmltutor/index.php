08658174b18b2a19a400437bf442bd6aac7b418a
charles <65935666+parhara@users.noreply.github.com>
2025-10-13 15:03:12 +0700

    Add files via upload

---
+++ xmltutor/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar File HTML Lokal</title>
+</head>
+<body>
+    <h1>List Tutorian</h1>
+
+    <?php
+            echo "List 1";
+    ?>    
+    
+    
+</body>
+</html>
